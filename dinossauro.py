347b1cb2421a237f7e75cd73666eec8a72d7e71b
Joao Lucas <jl.ice@hotmail.com>
2023-02-22 12:53:13 -0300

    Initial commit

---
+++ dinossauro.py
@@ -0,0 +1,17 @@
+print('''Aqui vai um Dinossauro:
+
+█████████
+█▄█████▄█
+█▼▼▼▼▼
+█
+█▲▲▲▲▲
+█████████
+██ ██
+
+E aqui vai um bolo! hummmmm''')
+print("i " *11)
+print("#"*21)
+print("="*21)
+print("#"*21)
+print("="*21)
+
